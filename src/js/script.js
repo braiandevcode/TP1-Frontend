239a3661d5698d256de06a324fd9dacdabdf94c3
Braian Palacios <braianpalacios699@gmail.com>
2024-07-19 03:53:59 -0300

    se definio estructura de pagina HTML y CSS para osterior continuidad.

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+// LISTA PRODUCTOS
+
+const listProducts =[];
+const listPrices=[];
+
+const createListProducts = ()=>{
+
+};
+/*
+<article class="card-product">
+            <h2 class="card-product__title">Pan</h2>
+            <div class="card-product__body">
+                <img src="" alt="">
+            </div>
+            <div class="card-product__footer">
+                <p class="card-product__description">$1000<sup>99</sup></p>
+            </div>
+        </article>
+
+*/ 
