e1ce9765ba066a5d4e40f470082bda540a4af9d4
Micaela Murrie <micaelamurrie@gmail.com>
2024-07-23 00:42:44 -0300

    Añadí pagina de contaco

--- src/js/script.js
+++ src/js/script.js
@@ -7,17 +7,17 @@ const iconWelcome = document.querySelector(".header-page__welcome");
 let hours = new Date().getHours();
 let textNode;
 // funcion auxiliar para crear nodo de texto.
-const createText = (el, text)=>{
+const createText = (el, text) => {
     textNode = document.createTextNode(text);
     el.append(textNode);
     return el;
 }
 // funcion auxiliar para agregar y remover clases, uso de parametros REST
-const updateClass= (el, classAdd, ...removeClass)=>{
-    if(el && classAdd){
-        if(removeClass.length == 0){
+const updateClass = (el, classAdd, ...removeClass) => {
+    if (el && classAdd) {
+        if (removeClass.length == 0) {
             el.classList.add(classAdd);
-        }else{
+        } else {
             el.classList.add(classAdd);
             removeClass.forEach(removeClassName => {
                 el.classList.remove(removeClassName);
@@ -26,14 +26,14 @@ const updateClass= (el, classAdd, ...removeClass)=>{
     }
 };
 
-if(hours >= 20){
+if (hours >= 5 && hours <= 12) {
+    createText(welcome, "¡Buenos Días!");
+    updateClass(iconWelcome, "bi-brightness-alt-high-fill", ["bi-cloud-sun-fill", "bi-cloud-moon-fill"]);
+} else if (hours >= 13 && hours <= 19) {
+    createText(welcome, "¡Buenas Tardes!");
+    updateClass(iconWelcome, "bi-cloud-sun-fill", ["bi-cloud-moon-fill", "bi-brightness-alt-high-fill"]);
+} else {
     createText(welcome, "¡Buenas Noches!");
-    updateClass(iconWelcome, "bi-cloud-moon-fill", "bi-cloud-sun-fill", "bi-brightness-alt-high-fill");
-}else if(hours < 20 && hours >= 13){
-    createText(welcome,"¡Buenas Tardes!");
-    updateClass(iconWelcome,  "bi-cloud-sun-fill", "bi-cloud-moon-fill", "bi-brightness-alt-high-fill");
-}else{
-    createText(welcome,"¡Buenos Días!");
-    updateClass(iconWelcome,  "bi-brightness-alt-high-fill", "bi-cloud-sun-fill", "bi-cloud-moon-fill");
+    updateClass(iconWelcome, "bi-cloud-moon-fill", ["bi-brightness-alt-high-fill", "bi-cloud-sun-fill"]);
 };
 
